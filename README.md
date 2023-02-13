4a17bee3748209513a520294f2ef2d17dc15cda8
georgeyoussif <gyoussif@getocto.io>
2023-02-13 21:15:01 +0200

    init project, create models, script to populate db

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# backend-challenge
+# Backend Challenge
+## Getting Started
+### Prerequisites
+Make sure you have a `pip` and `virtualenv` installed on your machine
+To install:
+  ```sh
+    sudo apt install python3-pip && pip3 install virtualenv
+  ```
+## Installation
+1. Clone the repo
+   ```sh
+   git clone https://github.com/gyoussif/backend-challenge.git
+   ```
+2. Create a virtual environment and activate it
+   ```sh
+    virtualenv venv && source {ABSOLUTE_PATH_TO_VENV}/venv/bin/activate  
+   ```
+
+   It's preferable to create the venv outside your workdir as it slows down the file processing of some IDEs.
+   However, if it works with you in any other way then that's great.
+
+2.  Install requirements using poetry
+    ```sh
+    poetry install
+    ```
+## Django Setup
+1. db migration
+    ```sh
+    python manage.py makemigrations #create migration files
+    python manage.py migrate #apply migration files on the db
+    ```
+2. populate db with random reviews
+    ```sh
+    python -m create_reviews 
+    ```
+3. creating a superuser (username:admin, password:admin)
+    ```sh
+    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@email.com', 'admin')" | python manage.py shell
+    ```
+4. run django server
+    ```sh
+    python manage.py runserver 8000      
+    ```
---
+++ create_reviews.py
@@ -0,0 +1,49 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apps.settings')
+django.setup()
+import random
+import datetime
+from django.utils import timezone
+from django.db import IntegrityError
+from apps.review import models
+
+questions_text = [
+    'What is your favorite color?',
+    'What is your favorite food?',
+    'What is your favorite movie?',
+    'What is your favorite song?']
+
+choices_text = [
+    ['Red', 'Blue', 'Green', 'Yellow'],
+    ['Pizza', 'Hamburger', 'Steak', 'Koshary'],
+    ['The Shawshank Redemption', 'The Godfather', 'The Dark Knight', 'Pulp Fiction'],
+    ['Bohemian Rhapsody', 'Stairway to Heaven', 'Imagine', 'Unstoppable']
+]
+
+def create_questions_and_choices():
+    for i in range(len(questions_text)):
+        question = models.Question.objects.create(text=questions_text[i])
+        for text in choices_text[i]:
+            choice=models.Choice.objects.create(text=text)
+            question.choices.add(choice)
+
+def create_answers():
+    questions = models.Question.objects.all()
+    for _ in range(4000):
+        question=random.choice(questions)
+        review = models.Review.objects.create(submitted_at=random_date())
+        choice = random.choice(question.choices.all())
+        models.Answer.objects.create(review=review, question=question, choice=choice)
+
+def random_date():
+    start = timezone.make_aware(datetime.datetime(2021, 1, 1))
+    end = timezone.make_aware(datetime.datetime(2023, 12, 31))
+    return timezone.make_aware(datetime.datetime.fromtimestamp(random.randint(start.timestamp(), end.timestamp())))
+
+try:
+    create_questions_and_choices()
+    create_answers()
+except IntegrityError as e:
+    print("IntegrityError:", e)
